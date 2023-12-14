69cee2b49db43853be7f5a5e0d311cdf012df6b4
MonoAii <141534913+monoaii@users.noreply.github.com>
2023-12-14 02:24:49 -0800

    Inject weather icon api

--- src/index.js
+++ src/index.js
@@ -1,49 +1,50 @@
-// change to current date and time
-let now = new Date();
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
-let months = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
-
-let currentTime = document.querySelector(".current-time");
-let currentDate = now.getDate();
-let currentDay = days[now.getDay()];
-let currentMonth = months[now.getMonth()];
-let currentHour = (now.getHours() < 10 ? "0" : "") + now.getHours();
-let currentMinute = (now.getMinutes() < 10 ? "0" : "") + now.getMinutes();
-
-currentTime.innerHTML = `${currentDay}, ${currentMonth} ${currentDate} | ${currentHour}:${currentMinute}`;
-
 //update weather data in realtime
 function updateWeatherData(response) {
+  let iconElement = document.querySelector("#current-weather-icon");
+  let cityElement = document.querySelector("#current-city");
+  let now = new Date(response.data.time * 1000);
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  let currentTime = document.querySelector(".current-time");
+  let currentDate = now.getDate();
+  let currentDay = days[now.getDay()];
+  let currentMonth = months[now.getMonth()];
+  let currentHour = (now.getHours() < 10 ? "0" : "") + now.getHours();
+  let currentMinute = (now.getMinutes() < 10 ? "0" : "") + now.getMinutes();
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = response.data.temperature.current;
-  let cityElement = document.querySelector("#current-city");
   let conditionDescriptionElement = document.querySelector(
     "#current-condition-des"
   );
   let feelsLike = document.querySelector("#feels-like-temp");
+
+  iconElement.innerHTML = `<img
+      src="${response.data.condition.icon_url}"
+      class="current-weather-icon"/>`;
 
   cityElement.innerHTML = response.data.city;
   conditionDescriptionElement.innerHTML = response.data.condition.description;
@@ -51,6 +52,8 @@ function updateWeatherData(response) {
   feelsLike.innerHTML = `Feels Like ${Math.round(
     response.data.temperature.feels_like
   )}`;
+
+  currentTime.innerHTML = `${currentDay}, ${currentMonth} ${currentDate} | ${currentHour}:${currentMinute}`;
 }
 
 //make api call and update temperature interface
